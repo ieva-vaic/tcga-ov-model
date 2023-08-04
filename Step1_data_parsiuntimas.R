1459e30ce5021f47920f25bd3ad61610d8bb51ee
ieva-vaic <ieva1118@gmail.com>
2023-08-04 16:36:03 +0200

    renamed scripts, cleaned up a bit, tried elastic net models on 2 non DGE normalizations

--- Step1_data_parsiuntimas.R
+++ Step1_data_parsiuntimas.R
@@ -1,42 +1,25 @@
+#This is the script no.1 required for downloading the TCGA data with TCGAbiolinks
 BiocManager::install("edgeR")
 BiocManager::install("genefilter")
 BiocManager::install("BioinformaticsFMRP/TCGAbiolinksGUI.data", force = TRUE)
 BiocManager::install("BioinformaticsFMRP/TCGAbiolinks", force = TRUE)
-# BiocManager::install("TCGAbiolinks") # and abobe doesnt work, installing related packages
-#install.packages(c("boot", "class", "cluster", "codetools", "foreign", "KernSmooth", "lattice", "MASS", "Matrix", "mgcv", "nlme", "nnet", "rpart", "spatial", "survival")) # did not work instaling packages thus installing dependaciens ‘biomaRt’, ‘GenomicRanges’, ‘XML’, ‘rvest’, ‘SummarizedExperiment’, ‘xml2’, ‘httr’
-#BiocManager::install("biomaRt")
-# ‘GenomicRanges’, ‘XML’, ‘rvest’, ‘SummarizedExperiment’, ‘xml2’, ‘httr’ 
-#BiocManager::install(c("XML", "rvest", "SummarizedExperiment", "xml2", "httr" ))
-#withr::with_envvar(c(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true"), remotes::install_github('BioinformaticsFMRP/TCGAbiolinks'))
-devtools::install_github("BioinformaticsFMRP/TCGAbiolinks")
-
+#devtools::install_github("BioinformaticsFMRP/TCGAbiolinks")
 # Load packages
 library("TCGAbiolinks")
-# library("limma")
-# library("edgeR")
-# library("glmnet")
-# library("factoextra")
-# library("FactoMineR")
-# library("caret")
 library("SummarizedExperiment")
-# library("gplots")
-# library("survival")
-# library("survminer")
-# library("RColorBrewer")
-# library("gProfileR")
-# library("genefilter")
 
 setwd("/home/ieva/tcga-ov-data/")
 GDCprojects = getGDCprojects()
-GDCprojects[c("project_id", "name")] #aš noriu TCGA-OV
+GDCprojects[c("project_id", "name")] #I choose ovarian cancer project TCGA-OV
 
 TCGAbiolinks:::getProjectSummary("TCGA-OV")
 
 query_TCGA1 = GDCquery(
   project = "TCGA-OV",
   data.category = "Transcriptome Profiling")
-View(getResults(query_TCGA1)) #čia sužinau apie visus OV mėginius
-
+#Find out about all transcriptomic data in TCGA project
+View(getResults(query_TCGA1)) 
+#Build your query
 query_TCGA = GDCquery(
   project = "TCGA-OV",
   data.category = "Transcriptome Profiling", # parameter enforced by GDCquery
@@ -44,35 +27,35 @@ query_TCGA = GDCquery(
   experimental.strategy = "RNA-Seq",
   workflow.type = "STAR - Counts", 
   access = "open" )
-
+#Chek your query
 View(getResults(query_TCGA))
-
-lihc_res = getResults(query_TCGA) # make results as table
+# Make results as table
+lihc_res = getResults(query_TCGA) 
+#Chek our your data
 head(lihc_res) # data of the first 6 patients.
 colnames(lihc_res) # columns present in the table
 unique(lihc_res$sample_type)
 summary(factor(lihc_res$sample_type))
-
-GDCdownload(query = query_TCGA, files.per.chunk = 200) #downloads GDCdata files, I moved it out afterwards
-
+#Download your data (do not forget to setwd to data folder, creates a big folder)
+GDCdownload(query = query_TCGA, files.per.chunk = 200) 
+#Prepare your data
 tcga_data <- GDCprepare(query_TCGA,
                         summarizedExperiment = TRUE)
-
+#Chek out final downloaded data
 dim(tcga_data)
-
 colnames(colData(tcga_data))
-pheno <- as.data.frame(colData(tcga_data))
-
+# Save clinical data to separate file
+pheno <- as.data.frame(colData(tcga_data)) #impotant to save clinical data
+# this is on original col data, but could be done on pheno:
+#count some most important clinical data
 table(tcga_data@colData$figo_stage)
-
 table(tcga_data@colData$vital_status)
-
+#Look at count data:
 dim(assay(tcga_data))
 head(assay(tcga_data)[,1:10])
 head(rowData(tcga_data))     # ensembl id and gene id of the first 6 genes.
-
-#final saved TCGA-OC data
-
+#############################################################################
+#final saved TCGA-OC data: full file RangedSummarizedExperiment tcga_data and clinical data (colData)
 # Save the data as a file, if you need it later, you can just load this file
 # instead of having to run the whole pipeline again
 saveRDS(object = tcga_data,
--- Step2_fixing_clinical_data_and_split.R
+++ Step2_fixing_clinical_data_and_split.R
@@ -1,5 +1,6 @@
-#scriptas kuris sutvarko phenodata i reikalingus stulpelius ir nusplitina mano trai. test ir weird setus
-#uzduotys:
+#This script is for: 1) cheking and filtering phenodata (clinical data) 
+#2) spliting data into rain, test nad weird sets
+#tasks in this script:
 #secondary navikus ir prior treatment atsiskirt i weird
 #atsiskiri jei 3 data train, test ir weird, kaip minimum vektoriai atskiri su vardais
 #parsinti ta keista  treatmentstulpeli 
@@ -10,34 +11,37 @@ library("caret")
 library("dplyr")
 library("tidyr")
 library("tidyverse")
-tcga_data = readRDS(file = "tcga_data.RDS") #pakeiciau pavadinima
+tcga_data = readRDS(file = "tcga_data.RDS") 
 pheno <- readRDS(file = "/home/ieva/tcga-ov-data//pheno.RDS")
 #####################################################################
-#apsiziuriu data
+#Chek the data 
 table(pheno$figo_stage, pheno$vital_status, useNA='a')
 table(pheno$days_to_death, pheno$vital_status, useNA='a')
 ggplot(pheno, aes(x=figo_stage  , y=days_to_death)) +
   geom_boxplot()
+############################################################
+#noninformative  columns (all na, all the same value):
+#tumor_descriptor, state, is_ffpe, tissue_type, days_to_diagnosis, 
+#last_known_disease_status, tissue_or_organ_of_origin, prior_malignancy,
+#classification_of_tumor, tumor_grade, progression_or_recurrence, 
+#alcohol_history, gender, ethnicity, disease_type, primary_site, project_id, 
+#name, releasable, released, days_to_sample_procurement, sample_type_id
+#informative colums (numerical);
+#intermediate_dimention, shortest_dimention, longest_dimention, days_to_last_follow_up,
+#age_at_diagnosis, year_of_diagnosis, age_at_index, days_to_birth, 
+# year_of_birth, days_to_death, year_of_death, initial_weight
+#informative colums (factors) figo_stage, race, vital_status
 
-#neinformatyvus stulpeliai (visi na, visi vienodi) tumor_descriptor, state, is_ffpe, tissue_type,
-#days_to_diagnosis, last_known_disease_status, tissue_or_organ_of_origin, prior_malignancy, 
-# classification_of_tumor, tumor_grade, progression_or_recurrence, alcohol_history, gender
-#ethnicity, disease_type, primary_site, project_id, name, releasable, released, days_to_sample_procurement, sample_type_id
-
-#informative stulpeliai (skaiciai) intermediate_dimention, shortest_dimention, longest_dimention,
-#days_to_last_follow_up, age_at_diagnosis, year_of_diagnosis, age_at_index, days_to_birth, 
-# year_of_birth, days_to_death, year_of_death, 
-#informative stulpeliai (factors) figo_stage, race, vital_status
-
-#susipiciaus visi like zemiau
+#Inspect susipiciaus colums:
+#synchonous maliganacy
 table(pheno$synchronous_malignancy, useNA='a')#pas mane irgi 2 buvo
-
+#morphology/diagnosis all three are the same
 table(pheno$primary_diagnosis, useNA='a') #1 cystadenocarcinoma ir 4 papillary visos HGSOC yra todel geriau neatsikirt
 table(pheno$morphology, useNA='a') #same as primary diagnosis 
 table(pheno$icd_10_code, useNA='a') #same as primary diagnosis
-
+#prior treatment
 table(pheno$prior_treatment, useNA='a') #tik vienas prior treatment #noriu ismest ta 1 zmogu
-
+# suspicous colums but not worth deletion:
 table(pheno$site_of_resection_or_biopsy, useNA='a') #tik 6 "Specified parts of peritoneum" 
 table(pheno$days_to_collection, useNA='a')  #Sample can be collected can be prospectively or retrospectively. This can be a negative value for samples taken retrospectively.
 table(pheno$initial_weight, useNA='a') #sample, specimen weight in grams
@@ -49,11 +53,13 @@ table(pheno$is_ffpe, useNA='a') #nors cia raso kad ner ffpe, bet paliksiu does n
 non_informative <- c("tumor_descriptor", "state", "is_ffpe", "tissue_type",
                      "days_to_diagnosis", "last_known_disease_status", "tissue_or_organ_of_origin", "prior_malignancy", 
                      "classification_of_tumor", "tumor_grade", "progression_or_recurrence", "alcohol_history", "gender",
-                     "ethnicity", "disease_type", "primary_site", "project_id", "name", "releasable", "released", "days_to_sample_procurement", "treatments", "sample_type_id", "composition")
+                     "ethnicity", "disease_type", "primary_site", "project_id", "name", "releasable",
+                     "released", "days_to_sample_procurement", "treatments", "sample_type_id", "composition")
 non_informative %in% colnames(pheno) # true if no spelling mistakes were made
-new_pheno <- pheno[ ,!names(pheno) %in% non_informative]
+new_pheno <- pheno[ ,!names(pheno) %in% non_informative] #new pheno data with no annoying colums
 ##############################################################################
-#reikia isparsinti treatments 
+#Parsing treatments colum
+#first convert the treatments colum to dataframe
 treatments_dataframe <- bind_rows(pheno$treatment, .id = "id")
 #reikia kad treatmentu dataframe butu vardai kurie po to sutaps su pheno
 treatments_dataframe$patient_names<- treatments_dataframe$submitter_id  
@@ -63,7 +69,7 @@ table(treatments_dataframe$days_to_treatment_end, useNA='a') # visi na, drop
 table(treatments_dataframe$days_to_treatment_start, useNA='a') # visi na, drop
 table(treatments_dataframe$treatment_id, useNA='a') # all unique, maybe drop
 table(treatments_dataframe$submitter_id, useNA='a') #perkeliau i patient names ir nusplitinau gala
-table(treatments_dataframe$treatment_type, useNA='a')  #kiekvienam zmogui yra po radiation ir parmasiutical eilute
+table(treatments_dataframe$treatment_type, useNA='a')  #kiekvienam zmogui yra po radiation ir Pharmaceutical eilute
 table(treatments_dataframe$regimen_or_line_of_therapy, useNA='a') # visi na, drop
 table(treatments_dataframe$treatment_effect, useNA='a')  # visi na, drop
 table(treatments_dataframe$therapeutic_agents, useNA='a')  # visi na, drop
@@ -71,33 +77,32 @@ table(treatments_dataframe$initial_disease_status, useNA='a')  # visi na, drop
 table(treatments_dataframe$treatment_intent_type, useNA='a')  # visi na, drop
 table(treatments_dataframe$treatment_anatomic_site, useNA='a')  # visi na, drop
 table(treatments_dataframe$treatment_outcome, useNA='a')  # visi na, drop
-table(treatments_dataframe$state, useNA='a')  # visi resleased, drop
+table(treatments_dataframe$state, useNA='a')  # visi released, drop
 drop_from_treamtment <- c("days_to_treatment_end", "days_to_treatment_start", "treatment_id",
                           "regimen_or_line_of_therapy", "treatment_effect", "therapeutic_agents", "initial_disease_status", 
                           "treatment_intent_type", "treatment_anatomic_site", "treatment_outcome", "state")
-drop_from_treamtment %in% colnames(treatments_dataframe) # true if no spelling mistakes were made
+drop_from_treamtment %in% colnames(treatments_dataframe) 
 treatments_dataframe <- treatments_dataframe[ ,!names(treatments_dataframe) %in% drop_from_treamtment]
 
 ###############################################################################
-#pasalinu be treatment
+#every person has 2 rows for treatments except for those who had no tretment
+#here I drop the rows where the reatment was not given (indicated by no/na in treatment_or_therapy)
 table(treatments_dataframe$treatment_or_therapy, useNA='a') #429 yes kiti no ar na, todel bandau nufiltruot tik yes
-had_treatment <- subset(treatments_dataframe, treatment_or_therapy == 'yes') #nudropinau
-table(had_treatment$patient_names, useNA='a') #rodo kad yra double kai kur
-had_treatment$patient <- had_treatment$patient_names #reikes po to sumerginimui
-
-#group_by(treatments_dataframe, patient_names) %>% summarize(ppp=paste(sort(treatment_or_therapy), collapse="_"), n=n()) %>% group_by(ppp) %>% summarize(n=n())# jul
-
+had_treatment <- subset(treatments_dataframe, treatment_or_therapy == 'yes') #new treatments dataframe
+table(had_treatment$patient_names, useNA='a') #this showes that some are double
+had_treatment$patient <- had_treatment$patient_names #add patient names for easy merging
+#paste treatment types to have 1 row per person
 had_treatment <- had_treatment %>% group_by(patient_names) %>% mutate(undergone_treatments=paste(sort(treatment_type), collapse="_"))   #galutinej eilutej treatment eiga
-had_treatment_collaped <- had_treatment[!duplicated(had_treatment$patient_names), ] # removinu duplications, susimazins dataframas
+had_treatment_collaped <- had_treatment[!duplicated(had_treatment$patient_names), ] # remove duplications, resulting smaller dataframe
 
 ################################################################################
-#merge treatments ir normal pheno data frame (sumazinta)
+#merge treatments and normal pheno data frame (final pheno dataframe)
 pheno_final <- merge(x = new_pheno, y = had_treatment_collaped, by = "patient", all = TRUE) 
-
-#nusplitint weird: definition ir treatments
+#############################################################################
+#split weird cases from the rest of the data by definition and treatments
 split_by_definition <- split(pheno_final, f = pheno_final$definition, drop = T)
-weird_cases <- split_by_definition$`Recurrent Solid Tumor` # weird grupe 7 zmones
-primary_tumor <- split_by_definition$`Primary solid Tumor`
+weird_cases <- split_by_definition$`Recurrent Solid Tumor` # weird group 7 people
+primary_tumor <- split_by_definition$`Primary solid Tumor` #new new phenodata
 
 split_prior_treatment <- split(primary_tumor, f = primary_tumor$prior_treatment, drop = T)
 primary_tumor <- split_prior_treatment$No
@@ -105,25 +110,39 @@ weird_cases <- rbind(weird_cases, split_prior_treatment$Yes)
 
 ###################################################################################
 # Set (random-number-generator) seed so that results are consistent between runs
-set.seed(18)
-train_ids <-rbinom(nrow(primary_tumor), size = 1, prob = 0.8) ==1
-#clinal data failai padalinti
-x_train = primary_tumor[train_ids, ]
-x_test  = primary_tumor[!train_ids, ]
-
-#save 3 failus i csv
+set.seed(18) #choose favorite number
+train_ids <-rbinom(nrow(primary_tumor), size = 1, prob = 0.8) ==1 #choose persentage
+#we split clinical data first (probably should be indicated by y instead of x)
+x_train = primary_tumor[train_ids, ] #new train data
+x_test  = primary_tumor[!train_ids, ] #new test data 
+#save 3 files in csv
 write.csv(weird_cases, "weird_cases.csv", quote = T, row.names = F) #normal quote F uzdedam bet kadangi kai kur gali but kableliu neveiks
-write.csv(x_train, "train_data.csv", , quote = T, row.names = F)
-write.csv(x_test, "test_data.csv", , quote = T, row.names = F )
+write.csv(x_train, "train_data.csv", quote = T, row.names = F)
+write.csv(x_test, "test_data.csv", quote = T, row.names = F )
 
+#split the rest of the data according to split clinical dataframes
+# for most normal analyses I want a matrix as it is given by assay function
+#option 1: go back and filter original matrix
+counts_ov <- assay(tcga_data)
 
-#o ner taip kad dabar tik clinical data atsiskyrem?
-#jauciu reiketu atsiskirti ir assay data
+train_counts2 <- counts_ov[, colnames(counts_ov) %in% train_barcodes]
+saveRDS(train_counts2, "train_count_matrix.RDS")
+
+#same with other count matrixes
+test_counts2 <- counts_ov[, colnames(counts_ov) %in% test_barcodes]
+saveRDS(test_counts2, "test_count_matrix.RDS")
+
+weird_counts2 <- counts_ov[, colnames(counts_ov) %in% weird_barcodes]
+saveRDS(weird_counts2, "weird_count_matrix.RDS") #this is weird because 1 variable too much
+
+#####################################################################################
+#spliting other kinds of count data just in case
+#assay data converted to dataframe
 counts_ov <- assay(tcga_data)
 counts_ov <- as.data.frame(t(counts_ov))
 counts_ov$barcode <- rownames(counts_ov)
 
-#counts filus sukuria 
+#counts files in dataframes 
 test_barcodes <- x_test[['barcode']]
 test_counts <- counts_ov[rownames(counts_ov) %in% test_barcodes, ]
 
@@ -138,6 +157,8 @@ write.csv(test_counts, "test_counts.csv", quote = F, row.names = F)
 write.csv(train_counts, "train_counts.csv", quote = F, row.names = F)
 write.csv(weird_counts, "weird_counts.csv", quote = F, row.names = F )
 
+####################################################################################
+#useless but left for reference if I need semijoin that filters while joining
 # #jsemioin counts and filtered clinical
 # counts_ov <- tibble::rownames_to_column(counts_ov, "row_names")
 # counts_ov$barcode <- counts_ov$row_names
@@ -160,20 +181,3 @@ write.csv(weird_counts, "weird_counts.csv", quote = F, row.names = F )
 # write.csv(weird_data_df, "weird_data_df.csv", quote = F, row.names = F )
 
 
-# for most normal analyses I want a matrix as it is given by assay function
-#option 1: go back and filter matrix
-counts_ov <- assay(tcga_data)
-# counts_ov <- as.data.frame(t(counts_ov))
-# counts_ov$barcode <- rownames(counts_ov)
-
-# train_barcodes <- x_train[['barcode']]
-# train_counts <- counts_ov[rownames(counts_ov) %in% train_barcodes, ]
-train_counts2 <- counts_ov[, colnames(counts_ov) %in% train_barcodes]
-saveRDS(train_counts2, "train_count_matrix.RDS")
-
-#same su kitais count matrixes
-test_counts2 <- counts_ov[, colnames(counts_ov) %in% test_barcodes]
-saveRDS(test_counts2, "test_count_matrix.RDS")
-
-weird_counts2 <- counts_ov[, colnames(counts_ov) %in% weird_barcodes]
-saveRDS(weird_counts2, "weird_count_matrix.RDS") #2ia wrid nes 1 variable tooooooo much
