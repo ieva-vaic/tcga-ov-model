cf9924b813354e166f745e1f63271497e666b7e7
ieva-vaic <ieva1118@gmail.com>
2023-09-20 17:28:23 +0200

    adding more heatmaps, and step 12: the test of models

--- mRNA-TCGA-OV/SideStep9_pictures.R
+++ mRNA-TCGA-OV/SideStep9_pictures.R
@@ -4,6 +4,8 @@ library(viridis)
 library(reshape2)
 setwd("~/rprojects/TCGA-OV-data") #wsl
 library(tidyverse)
+library(RColorBrewer)
+library('DESeq2')
 #because the normalized hplot was bad this might not be the most appropirate?
 gtex_counts_train <- readRDS("train_gtcga_normcounts_prot.RDS")
 gtex_counts_train <- data.frame(gtex_counts_train)
@@ -16,18 +18,6 @@ gtex_counts_train %>%
   geom_jitter(color="black", size=0.4, alpha=0.9) +
   xlab("Study")+
   ylab(substitute(paste(italic("TTC4"))))
-
-
-gtex_tcga_genes <- c( "TTC4"  ,  "SLC39A1" ,"TMEM110", "RAD50" ,  "ANKHD1"  ,"ZBTB9" ,  "RPS10" ,
-   "CLDN4" ,  "PFDN5" ,  "PAGR1"  ,"RNASEK" , "GPS2"   , "RTEL1" , "grupe")
- gtex_counts_train %>% select(gtex_tcga_genes) %>%
-  pivot_longer(., cols = c(gtex_tcga_genes[1:13]), names_to = "Genes", values_to = "EXPR") %>%
-  ggplot(aes(x = Genes, y = EXPR, fill = grupe)) +
-  geom_boxplot()+
-  ylab("Normalized expression")+
-  theme(
-     axis.text.x = element_text(face = "italic"))+
-   guides(fill=guide_legend(title="Study"))
  
  mano_genes <- c( "PPP2R1A" ,"ARID1A", "CTNNB1", "FBXW7",  "NOTCH1" ,"NOTCH2", "NOTCH3" ,  "NOTCH4"  ,
                           "HES1"  ,"JAG2" , "DLL1",  "HOPX" , "grupe")
@@ -39,7 +29,6 @@ gtex_tcga_genes <- c( "TTC4"  ,  "SLC39A1" ,"TMEM110", "RAD50" ,  "ANKHD1"  ,"ZB
    theme(
      axis.text.x = element_text(face = "italic"))+
    guides(fill=guide_legend(title="Study"))
-
 
  pheno_train <- readRDS("tcga_pheno_train.RDS")
  tcga_counts_train <- readRDS("tcga_counts_train.RDS")
@@ -104,7 +93,6 @@ tcga %>% select(vital_genes) %>%
 
 ############################################################################
 #heatmap between TCGA and GTEX
-library(RColorBrewer)
 gene.matrix <- as.matrix(gtex_counts_train[1:13680])
 dim(gene.matrix)
 # Example: grouping from the first letter:
@@ -131,3 +119,102 @@ dim(coxnet.matrix) #489  33 -> 2 pamesti bes reikejo keist vardus vienas FAM83H_
 jpeg(file="figures/heatmap_coxnet.jpeg", height=700, width=700) #išsaugijimui didesniu formatu
 heatmap(coxnet.matrix, Colv = NA, scale="col", col=rev(brewer.pal(9,"RdBu")), RowSideColors=colSide)
 dev.off()
+################################################################################
+#side step Heatmaps pries ir po normalizavimo
+#part1: pries norm
+counts_gtcga <- readRDS("gtcga_final_counts2.RDS") 
+counts_gtcga <- data.matrix(counts_gtcga) #19193   596
+which(colnames(counts_gtcga)=="TCGA-13-1499-01A-01R-1565-13") #297
+counts_gtcga <- counts_gtcga[, -297] #19193   595 liko 
+snames = colnames(counts_gtcga)
+group = substr(snames, 1, 4) 
+group <- as.numeric(as.factor(group))
+colSide <- brewer.pal(9, "Set1")[group] #dabar row zmones turetu buy
+counts_gtcga <- t(counts_gtcga)
+#clusters from both sides no norm
+png(file="figures/nonorm_clustbothsides.png", height=700, width=700) #išsaugijimui didesniu formatu
+heatmap(counts_gtcga, scale="col", col=rev(brewer.pal(9,"RdBu")), RowSideColors=colSide)
+dev.off()
+#part1: po norm
+nomcounts_gtcga <- readRDS("mrna_voom_protein.RDS")
+nomcounts_gtcga <- t(nomcounts_gtcga)
+png(file="figures/normcountsheatmap.png", height=700, width=700) #išsaugijimui didesniu formatu
+heatmap(nomcounts_gtcga, Colv = NA, scale="col", col=rev(brewer.pal(9,"RdBu")), RowSideColors=colSide)
+dev.off()
+#clusters from both sides: normcounts
+png(file="figures/normcountsheatmap2.png", height=700, width=700) #išsaugijimui didesniu formatu
+heatmap(nomcounts_gtcga, scale="col", col=rev(brewer.pal(9,"RdBu")), RowSideColors=colSide)
+dev.off()
+################################################################################
+#side 2 PCA?
+group_f <- factor(group, levels = c("GTEX", "TCGA"))
+colType <- c("blue", "red2")[group_f]
+pchType <- c(18, 16)[group_f]
+pca <- prcomp(nomcounts_gtcga)
+plot(
+  pca$x,
+  col = colType,
+  cex = 1.0)
+legend(
+  "bottomright",
+  bty = "n",
+  c("GTEX", "TCGA"),
+  fill = c("blue", "red2"),
+  cex = 1.0)
+################################################################################
+#side 3 volcano plot: p-value agains log-fold change
+#tam reikia log foldu ir p-values, tai suskaiciuos man DESEQ2 bet reikia ant raw
+#counts_gtcga 
+#i need description table sample names and gtcga or gtex
+coldata <- data.frame(snames, group)
+counts_gtcga <- t(counts_gtcga) #colums are patients
+coldata$group <- 
+  factor(coldata$group, levels = c("GTEX", "TCGA"))
+dds <- DESeqDataSetFromMatrix(countData = counts_gtcga,
+                              colData = coldata,
+                              design= ~ 0 + group)
+dds <- DESeq(dds)
+resultsNames(dds) # lists the coefficients
+# or to shrink log fold changes association with condition: (suggested by package)
+res <- lfcShrink(dds, coef="groupTCGA", type="apeglm") #nezinau ar cia ok TCGA naudot
+res_df <- as.data.frame(res) 
+#volcano plot I wanted
+par(mar=c(5,5,5,5), cex=1.0, cex.main=1.4, cex.axis=1.4, cex.lab=1.4)
+#Adjusted P values (FDR Q values)
+with(res_df, plot(log2FoldChange, -log10(padj), pch=20, main="Volcano plot", cex=1.0, xlab=bquote(~Log[2]~fold~change), ylab=bquote(~-log[10]~Q~value)))
+with(subset(res_df, padj<0.05 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(padj), pch=20, col="red", cex=0.5))
+#Add lines for absolute FC>2 and P-value cut-off at FDR Q<0.05
+abline(v=0, col="black", lty=3, lwd=1.0)
+abline(v=-2, col="black", lty=4, lwd=2.0)
+abline(v=2, col="black", lty=4, lwd=2.0)
+abline(h=-log10(max(res_df$pvalue[res_df$padj<0.05], na.rm=TRUE)), col="black", lty=4, lwd=2.0)
+###############################################################################
+#redoing pictures: only the train normalized data
+#First I wanna see the top gene (The ~35 coxnet after elastic net) boxplots 
+gtex_counts_train <- readRDS("train_gtcga_normcounts_prot.RDS")
+gtex_counts_train <- data.frame(gtex_counts_train)
+gtex_counts_train$grupe <- substr(rownames(gtex_counts_train), 1, 4)
+gtex_counts_train$grupe <- as.factor(gtex_counts_train$grupe)
+#filter for ~35 coxnet after getting the train data
+res_coef_cox_names <- read.csv("res_coef_coxnet_names.csv")
+res_coef_cox_names <- res_coef_cox_names$x
+res_coef_cox_names <- c(res_coef_cox_names, "grupe") #cia reik priappendint kad nufiltruotu ka reik
+#now for the boxplots
+gtex_counts_train %>% select(res_coef_cox_names) %>%
+  pivot_longer(., cols = c(res_coef_cox_names[1:38]), names_to = "Genes", values_to = "EXPR") %>%
+  ggplot(aes(x = Genes, y = EXPR, fill = grupe)) +
+  geom_boxplot()+
+  ylab("Normalized expression")+
+  theme(
+    axis.text.x = element_text(face = "italic", angle=-90))+
+  guides(fill=guide_legend(title="Study"))
+suvROCgenes <- c("grupe", "TTC4", "RAD50", "RPS10", 
+                 "CLDN4", "ARPC1B", "PKP3", "GRB7")
+gtex_counts_train %>% select(suvROCgenes) %>%
+  pivot_longer(., cols = c(suvROCgenes[2:8]), names_to = "Genes", values_to = "EXPR") %>%
+  ggplot(aes(x = Genes, y = EXPR, fill = grupe)) +
+  geom_boxplot()+
+  ylab("Normalized expression")+
+  theme(
+    axis.text.x = element_text(face = "italic", angle=-90))+
+  guides(fill=guide_legend(title="Study"))
---
+++ mRNA-TCGA-OV/Step12_TEST.R
@@ -0,0 +1,118 @@
+#STEP LAST - test againts test data
+#1. ELASTIC NET GTEX vs TCGA
+#2. COXNET SURVIVAL!
+library(glmnet)
+library(yardstick)
+#1st WOUPS teks perrunint modeli, biski kitaip everytime
+gtex_counts_train <- readRDS("train_gtcga_normcounts_prot.RDS")
+gtex_counts_test <- readRDS("test_gtcga_normcounts_prot.RDS")
+gtex_counts_train <- data.matrix(gtex_counts_train)
+gtex_counts_test <- data.matrix(gtex_counts_test)
+snames = rownames(gtex_counts_train);
+group = substr(snames, 1, 4); #Sets up level information for samples.
+group = as.factor(group)
+#Model selection, lasso (no weak values left)
+#using norm.counts
+#clinical feature: gtex or TCGA data
+res_gtex = cv.glmnet(
+  x = gtex_counts_train,
+  y = group,
+  alpha = 0.5,
+  family = "binomial"
+)
+res_gtex #atrenka 219 this time
+plot(res_gtex)
+# Getting genes that contribute for the prediction
+res_coef_gtex = coef(res_gtex, s="lambda.min") # the "coef" function returns a sparse matrix
+head(res_coef_gtex) # in a sparse matrix the "." represents the value of zero
+# get coefficients with non-zero values
+res_coef_gtex = res_coef_gtex[res_coef_gtex[,1] != 0,] 
+res_coef_gtex = res_coef_gtex[-1]
+res_coef_gtex_names = names(res_coef_gtex) # get names of the (non-zero) variables.
+res_coef_gtex_names 
+saveRDS(res_gtex, "ELASTIC_NET_219_modelis.RDS") #for reproducibility cia 
+################################################################################
+# Test/Make prediction on test dataset
+y_pred = predict(res_gtex, newx=gtex_counts_test, type="class", s="lambda.min")
+length(y_pred) #106
+snamest <- rownames(gtex_counts_test)
+groupt <-substr(snamest, 1, 4)
+y_test <- data.frame(snamest, groupt)
+rownames(y_test) <- y_test$snamest
+y_test <- as.matrix(y_test)
+y_test <- y_test[,-1]
+y_test <- as.matrix(y_test)
+confusion_matrix = table(y_pred, y_test) 
+confusion_matrix #niekas nieko neconfusina.
+print(paste0("Precision: ",precision(confusion_matrix)))
+y_pred = predict(res_gtex, type="response", newx =gtex_counts_test )
+pred_class <- y_pred >0.5
+test_class <- y_test == "TCGA"
+v <- pred_class == test_class
+sum(v) / nrow(y_pred) #loss accuracy?
+###############################################################################
+#coxnet redo for test
+gtex_counts_train <- readRDS("train_gtcga_normcounts_prot.RDS")
+gtex_genes <- readRDS("gtcga_elastic.RDS")
+gtex_filtered_counts_train <- gtex_counts_train[colnames(gtex_counts_train) %in% gtex_genes] 
+
+gtex_filtered_counts_train2 <- as.data.frame(t(gtex_filtered_counts_train))
+gtex_filtered_counts_train2 <- gtex_filtered_counts_train2 %>%  dplyr::select(starts_with("TCGA")) 
+dim(gtex_filtered_counts_train2)
+gtex_filtered_counts_train2 <- as.data.frame(t(gtex_filtered_counts_train2))
+#336 samples #221 genes
+
+#clinical data
+pheno_train <- readRDS("tcga_pheno_train.RDS")
+dim(pheno_train)
+#336 samples, 69 variables
+
+# we are only interested in the "Primary solid Tumor" cases for survival
+clin_df = pheno_train[,
+                      c("barcode",
+                        "vital_status",
+                        "days_to_death",
+                        "days_to_last_follow_up",
+                        "neoplasmhistologicgrade",
+                        "figo_stage")]
+
+# create a new boolean variable that has TRUE for dead patients and FALSE for live patients
+clin_df$deceased = clin_df$vital_status == "Dead"
+
+# create an "overall survival" variable that is equal to days_to_death
+# for dead patients, and to days_to_last_follow_up for patients who
+# are still alive
+clin_df$overall_survival = ifelse(clin_df$deceased,
+                                  clin_df$days_to_death,
+                                  clin_df$days_to_last_follow_up)
+
+#add all genes to clin_df
+gtex_filtered_counts_train2$barcode <- rownames(gtex_filtered_counts_train2)
+clin_df_joined <- left_join(clin_df, gtex_filtered_counts_train2, by = "barcode")
+colnames(clin_df_joined) <- gsub("[.]", "_", colnames(clin_df_joined)) 
+colnames(clin_df_joined) <- gsub("[-]", "_", colnames(clin_df_joined)) 
+#COXNET!
+clin_df_joined2 <- clin_df_joined %>% drop_na(overall_survival) %>% drop_na(deceased) 
+clin_df_joined2$decesed2 <- as.integer(as.logical(clin_df_joined2$deceased))
+time <- clin_df_joined2$overall_survival
+status <- clin_df_joined2$decesed2
+y2 <- clin_df_joined2[ ,c(8,7)]
+names(y2) <- c("time", "status")
+y2 <- as.matrix(y2)
+head(y2)
+surv_counts <- clin_df_joined2[, 9:227]
+surv_counts <- as.matrix(surv_counts)
+cox_fitx <- glmnet(surv_counts, y2, family="cox")
+cox_fitx
+plot(cox_fitx)
+coef_x <- coef(cox_fitx, s = 0.05)
+head(coef_x)
+coef_x = coef_x[coef_x[,1] != 0,] 
+res_coef_cox_names = names(coef_x) # get names of the (non-zero) variables.
+res_coef_cox_names #38
+#cross-validation using the Harell C index. Note that unlike most error measures, a higher C index means better prediction performance
+set.seed(1)
+cvfit <- cv.glmnet(surv_counts, y2, family = "cox", type.measure = "C")
+plot(cvfit)
+cvfit$lambda.min
+cvfit$lambda.1se
